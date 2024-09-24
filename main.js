c29dcdc13a08d65b3a9180370411702ad5144171
vaisefo <gabriela.stigar@escola.pr.gov.br>
2024-09-24 08:45:52 -0300

    .

---
+++ main.js
@@ -0,0 +1,5 @@
+function criaCartao(categoria,pergunta,resposta){
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = ` `
---
+++ perguntas.js
@@ -0,0 +1,99 @@
+criaCartao(
+'História',
+'Por que o bandeirantismo não aflorou no norte e no sul do Brasil, mas no centro-oeste?',
+'Porque no norte e sul havia alta presença da Coroa, no centro oeste a Coroa só olhou depois da descoberta do ouro.')
+
+criaCartao(
+'História',
+'Ao longo do período colonial, qual atividade foi mais lucrativa?',
+'Agrícola.')
+
+criaCartao(
+'História',
+'O que foi a Mesopotâmia?',
+'Onde houve as primeiras civilizações;com leis,Estado, matemática, astronomia,roda,escrita cuneiforme, agricultura e cidades.Muito disputada por estar entre os três continentes.')
+
+criaCartao(
+'História',
+'O que era um bárbaro e um meteco na Grécia?',
+'Bárbaro era um estrangeiro.Meteco era um estrangeiro de outra pólis.')
+
+criaCartao(
+'História',
+'O que era uma polis?',
+'Polis era uma cidade Estado. Hélade é um conjunto de polis com mesma cultura, língua,humanistas, mitológicos ,havia escravidão e podiam ser aliados ou inimigos entre si.')
+
+criaCartao(
+'História',
+'O que caracterizou o período clássico grego?',
+'Desenvolvimento econômico e expansão persa que trouxe as guerras pérsicas ou médicas')
+
+criaCartao (
+'História',
+'Os jesuítas eram contra ou a favor da escravização dos índios?',
+'Contra, queriam fazer deles seu domínio religioso e ideológico.')
+
+criaCartao(
+'Biologia',
+'Qual o nome dado ao cruzamento de espécies diferentes?',
+'Híbridos.(a espécie Zebróide é geralmente uma raça nascida do cruzamento entre uma zebra e qualquer espécie de eqüino).')
+
+criaCartao(
+'Biologia',
+'Estruturas análogas derivam de qual processo?',
+'Convergência evolutiva.')
+
+criaCartao(
+'Física',
+'Quais os dois tipos de fusível?',
+'Fio de chumbo: É um metal de baixo ponto de fusão. Se a intensidade da corrente ultrapassar o limite o fio de chumbo se aquece e se funde. Disjuntor: É usado como fusível e como chave. Se A corrente ultrapassar o limite, desliga sozinho.')
+
+criaCartao(
+'Física',
+'Qual a importância da chave no circuito?',
+'Permitir(chave fechada) ou não (chave aberta) a passagem de corrente elétrica num determinado fio condutor de eletricidade. Geralmente é representada por K.')
+
+criaCartao(
+'Física',
+'O que é um reostato?',
+'Um resistor que possui resistência elétrica variável, em função da mudança de posição de um cursor.')
+
+criaCartao(
+'Física',
+'Qual a diferença entre as ligações em série e em paralelo?',
+'O resistor de maior resistência elétrica dissipa mais potência que os outros ( se ligado em série) e menos (se ligado em paralelo).')
+
+criaCartao(
+'Física',
+'O que é um resistor?',
+'É um aparelho no qual ocorre o efeito Joule, que consiste na transformação de energia elétrica exclusivamente em energia térmica.')
+
+criaCartao(
+'Física',
+'De onde surgiu o nome efeito Joule?',
+'A partir da relação entre as energias mecânica, elétrica e térmica.')
+
+criaCartao(
+'Física',
+'Qual a equação utilizada para calcular potência elétrica?',
+'P=U•i.')
+
+criaCartao(
+'Física',
+'Para que pode ser usado um reostato?',
+'Controle de lâmpada (abajur), rotação do ventilador, intensidade sonora(rádio),etc.'')
+
+criaCartao(
+'Física',
+'KWh é uma unidade do que?',
+'Energia.')
+
+criaCartao(
+'Química',
+'Qual é o elemento mais eletronegativo?',
+'F.')
+
+criaCartao(
+'Química',
+'Por qual princípio é regido o equilíbrio químico ?',
+'Princípio de le chateliner.')
