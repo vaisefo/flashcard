743ae757093cd0d7fa4c8c7e715eb1dd995cbe09
vaisefo <gabriela.stigar@escola.pr.gov.br>
2024-10-01 07:31:52 -0300

    .

--- main.js
+++ main.js
@@ -2,4 +2,19 @@ function criaCartao(categoria,pergunta,resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-    cartao.innerHTML = ` `
+    cartao.innerHTML = `<div class="cartao-conteudo">
+    <h3> ${categoria} </h3>
+    
+    <div class="cartao-pergunta">
+        <p>${pergunta}</p>
+        </div>
+
+        <div class="cartao-resposta">
+        <p>${resposta}</p>
+        </div>
+        </div>
+        `
+
+        container.appendChild(cartao);
+
+}
